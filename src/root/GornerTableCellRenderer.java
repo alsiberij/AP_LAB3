8af22d2a9dfd54e6151819c064d1ab5e8741c645
alsiberij <alsiberij@gmail.com>
2020-11-21 17:21:00 +0300

    Inner field added, initialization block added

--- src/root/GornerTableCellRenderer.java
+++ src/root/GornerTableCellRenderer.java
@@ -1,7 +1,29 @@
 package root;
 
+import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 
 public class GornerTableCellRenderer implements TableCellRenderer {
+
+    private String requiredValue;
+
+    private Double rangeBeg;
+    private Double rangeEnd;
+
+    private JPanel panel = new JPanel();
+    private JLabel label = new JLabel();
+
+    private DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+
+    {
+        formatter.setMaximumFractionDigits(6);
+        formatter.setGroupingUsed(false);
+        DecimalFormatSymbols curSymbol = formatter.getDecimalFormatSymbols();
+        curSymbol.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(curSymbol);
+    }
     
 }
