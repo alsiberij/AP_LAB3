fbdd243a84766a572065afcad2a03bcce29d79ed
alsiberij <alsiberij@gmail.com>
2020-11-21 17:46:07 +0300

    Inner fields added

--- src/root/GornerTableFrame.java
+++ src/root/GornerTableFrame.java
@@ -4,4 +4,29 @@ import javax.swing.*;
 
 public class GornerTableFrame extends JFrame {
 
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+
+    private double[] coefficients;
+
+    private JFileChooser fileChooser = null;
+
+    private JMenuItem saveToTxtMI;
+    private JMenuItem saveToBinMI;
+    private JMenuItem saveToCsvMI;
+    private JMenuItem searchValueMI;
+    private JMenuItem searchValueFromRangeMI;
+    private JMenuItem infoMI;
+
+    private JTextField xBegTF;
+    private JTextField xEndTF;
+    private JTextField stepTF;
+
+    private Box hResultTableBox;
+
+    private GornerTableCellRenderer renderer = new GornerTableCellRenderer();
+
+    private GornerTableModel dataTable;
+
+    
 }
