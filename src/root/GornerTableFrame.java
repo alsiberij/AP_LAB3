8297ddddb375a1f2c9c76fa3b61d25cdc2cf25d9
alsiberij <alsiberij@gmail.com>
2020-11-21 18:26:56 +0300

    Implemented: constructMenu()

--- src/root/GornerTableFrame.java
+++ src/root/GornerTableFrame.java
@@ -2,6 +2,8 @@ package root;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class GornerTableFrame extends JFrame {
 
@@ -17,7 +19,7 @@ public class GornerTableFrame extends JFrame {
     private JMenuItem saveToCsvMI;
     private JMenuItem searchValueMI;
     private JMenuItem searchValueFromRangeMI;
-    private JMenuItem infoMI;
+    private JMenuItem showInfoMI;
 
     private JTextField xBegTF;
     private JTextField xEndTF;
@@ -44,7 +46,105 @@ public class GornerTableFrame extends JFrame {
     }
 
     private void constructMenu() {
-        //TODO
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Файл");
+        JMenu tableMenu = new JMenu("Таблица");
+        JMenu infoMenu = new JMenu("Справка");
+
+        menuBar.add(fileMenu);
+        menuBar.add(tableMenu);
+        menuBar.add(infoMenu);
+
+        Action saveToTxtAction = new AbstractAction("Сохранить в .txt") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(GornerTableFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    //SaveToTxt(fileChooser.getSelectedFile())
+                }
+            }
+        };
+        saveToTxtMI = fileMenu.add(saveToTxtAction);
+        saveToTxtMI.setEnabled(false);
+
+        Action saveToBinAction = new AbstractAction("Сохранить в .bin") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(GornerTableFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    //SaveToBin(fileChooser.getSelectedFile())
+                }
+            }
+        };
+        saveToBinMI = fileMenu.add(saveToBinAction);
+        saveToBinMI.setEnabled(false);
+
+        Action saveToCsvAction = new AbstractAction("Сохранить в .csv") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser == null) {
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(GornerTableFrame.this) == JFileChooser.APPROVE_OPTION) {
+                    //SaveToCsv(fileChooser.getSelectedFile())
+                }
+            }
+        };
+        saveToCsvMI = fileMenu.add(saveToCsvAction);
+        saveToCsvMI.setEnabled(false);
+
+        Action searchValueAction = new AbstractAction("Найти значение") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String value = JOptionPane.showInputDialog(GornerTableFrame.this, "Введите значение", "Поиск", JOptionPane.QUESTION_MESSAGE);
+                renderer.setRequiredValue(value);
+                repaint();
+            }
+        };
+        searchValueMI = tableMenu.add(searchValueAction);
+        searchValueMI.setEnabled(false);
+
+        Action searchValueFromRangeAction = new AbstractAction("Найти значение из диапазона") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String rangeBegStr = JOptionPane.showInputDialog(GornerTableFrame.this, "Введите начало отрезка", "Поиск", JOptionPane.QUESTION_MESSAGE);
+                String rangeEndStr = JOptionPane.showInputDialog(GornerTableFrame.this, "Введите конец отрезка", "Поиск", JOptionPane.QUESTION_MESSAGE);
+                Double rangeBeg = null;
+                Double rangeEnd = null;
+                try {
+                    rangeBeg = Double.parseDouble(rangeBegStr);
+                    rangeEnd = Double.parseDouble(rangeEndStr);
+                } catch (NumberFormatException exception) {
+                    JOptionPane.showMessageDialog(GornerTableFrame.this, "Неверные данные");
+                    return;
+                }
+                renderer.setRangeBeg(rangeBeg < rangeEnd ? rangeBeg : rangeEnd);
+                renderer.setRangeEnd(rangeBeg < rangeEnd ? rangeEnd : rangeBeg);
+
+                repaint();
+            }
+        };
+        searchValueFromRangeMI = tableMenu.add(searchValueFromRangeAction);
+        searchValueFromRangeMI.setEnabled(false);
+
+        Action showInfoAction = new AbstractAction("О программе") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ImageIcon icon = new ImageIcon(new ImageIcon(GornerTableFrame.class.getResource("icon.jpg")).getImage().getScaledInstance(150, 200, Image.SCALE_DEFAULT));
+                JOptionPane.showMessageDialog(GornerTableFrame.this, "Alexander Sibirtsev, BSU", "Справка", JOptionPane.PLAIN_MESSAGE, icon);
+            }
+        };
+        showInfoMI = infoMenu.add(showInfoAction);
+
+        setJMenuBar(menuBar);
     }
 
     private void constructTop() {
